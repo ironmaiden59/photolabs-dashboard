e539651090e6930a1260dd377989a4ae92b8b8fc
ralph <ralphdcouto1987@gmail.com>
2023-09-15 21:17:33 -0400

    included loading component and panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,58 @@
 import React, { Component } from "react";
-
+import Loading from "./Loading";
 import classnames from "classnames";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza"
+  }
+];
+
 
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = data.map(panel => (
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+      />
+    ));
+
+    return (
+      <main className={dashboardClasses}>
+        {panels} {/* Render the panels array here */}
+      </main>
+    );
   }
 }
 
